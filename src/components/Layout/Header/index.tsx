64b87e53e5f99b5a29cc3ee39ba63a98ebde3b35
syed.ahamed <syed.ahamed@MGLXINMAC9K3R7.local>
2026-02-02 00:37:32 +0530

    diable:page bounces

--- src/components/Layout/Header/index.tsx
+++ src/components/Layout/Header/index.tsx
@@ -46,7 +46,7 @@ const Header: React.FC = () => {
 
   return (
     <header
-      className={`fixed top-0 z-40 w-full pb-5 transition-all duration-300 ${sticky ? ' shadow-lg bg-darkmode pt-5' : 'shadow-none md:pt-14 pt-5'
+      className={`fixed top-0 z-40 w-full pb-5 transition-all duration-300 ${sticky ? ' shadow-lg bg-darkmode pt-5' : 'shadow-none md:pt-14 pt-5 bg-darkmode/60 backdrop-blur-md'
         }`}>
       <div className='lg:py-0 py-2'>
         <div className='container px-4 flex items-center justify-between px-4'>
