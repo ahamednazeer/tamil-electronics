77a30142b1573881171cacec7868b43341a3003b
syed.ahamed <syed.ahamed@MGLXINMAC9K3R7-43.local>
2026-03-29 23:16:50 +0530

    feat:analytics vercel

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,7 @@ import FloatingWhatsAppButton from '@/components/common/FloatingWhatsAppButton'
 import { GoogleAnalytics } from '@next/third-parties/google'
 import { LenisProvider } from '@/components/common/ReactLenis'
 import Script from 'next/script'
+import { Analytics } from '@vercel/analytics/react'
 
 // Optimize font loading with display swap and preload
 const font = DM_Sans({
@@ -149,6 +150,7 @@ export default function RootLayout({
               </Aoscompo>
               <FloatingWhatsAppButton />
               <ScrollToTop />
+              <Analytics />
             </LanguageProvider>
           </LenisProvider>
         </ThemeProvider>
